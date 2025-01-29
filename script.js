a18472c0e5f7339c6336d2640feacd97bd271b71
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-01-29 23:19:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your code here
+// //your code here
+// <!DOCTYPE html>
+
+// <html>
+
+// <head>
+// 	<link rel="stylesheet" href="styles.css">
+// </head>
+
+// <body>
+// 	<!-- the entire body must be written by student -->
+//     <!-- <script type="text/javascript" src="./script.js">
+// </script> -->
+// 	<div>
+// This is a demo of box model, try to replicate this box model in Html & Css and pass all the test cases.		
+// 	</div>
+	
+	
+
+// </body>
+
+// </html>
